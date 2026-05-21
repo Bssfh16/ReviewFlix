93dbdcffd3aa43098abb2b7e993530a84b58769a
Hisham <boussofhisham@gmail.com>
2026-05-22 01:30:51 +0200

    breeze geïnstalleerd voor authentificatie en link naar pages teruggeplaatst

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\MediaController;
 use App\Http\Controllers\FaqController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ContactController;
+
+
 
 Route::get('/', function () {
     return view('pages.home');
@@ -25,8 +28,14 @@ Route::get('/contact', [ContactController::class, 'index']);
 
 Route::post('/contact', [ContactController::class, 'store']);
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
