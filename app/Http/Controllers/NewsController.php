da915eb601f7acb29255b8c6c02a25b1e0e3157a
Hisham <boussofhisham@gmail.com>
2026-05-21 20:04:46 +0200

    controller aangemaakt en logica geschreven en route vastgelegd en getest

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\NewsItem;
 
-class NewsController extends Controller
+class NewsController extends Controller 
 {
-    //
+    public function index() {
+        {
+            $news = NewsItem::all();
+            return $news;
+        }    
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
 
 Route::view('/', 'welcome')->name('home');
+
+Route::get('/news', [NewsController::class, 'index']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
