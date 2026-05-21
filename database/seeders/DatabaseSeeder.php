c852499849ddac32560da84637757f827c09e8c6
Hisham <boussofhisham@gmail.com>
2026-05-21 16:59:25 +0200

    aanpassingen (runfunctie en aantal voor het aanmaken models toegevoegd in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin user',
+            'username' => 'admin',
+            'email' => 'admin@ehb.be',
+            'password' => 'Password!321',
+            'is_admin' => true,
         ]);
+
+        User::factory(5)->create();
+
+        NewsItem::factory(5)->create();
+
+        MediaItem::factory(5)->create();
+
+        FaqCategory::factory(5)->create();
+
+        FaqItem::factory(5)->create();
+
+        Review::factory(5)->create();
     }
 }
