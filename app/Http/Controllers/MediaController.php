e0a257b996206c07845934e66b9ca22116f37696
Hisham <boussofhisham@gmail.com>
2026-05-21 20:18:16 +0200

    controller aangemaakt en logica geschreven en route vastgelegd en getest voor MediaItem

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MediaItem;
+
 
 class MediaController extends Controller
 {
-    //
-}
+    public function movies() {
+        $movies = MediaItem::where('type', 'Movie')->get(); 
+        return $movies;
+    }
+
+    public function series() {
+        $series = MediaItem::where('type', 'Serie')->get(); 
+        return $series;
+    }
+
+}    
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
+use App\Http\Controllers\MediaController;
 
 Route::view('/', 'welcome')->name('home');
 
 Route::get('/news', [NewsController::class, 'index']);
+
+Route::get('/movies', [MediaController::class, 'movies']);
+
+Route::get('/series', [MediaController::class, 'series']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
