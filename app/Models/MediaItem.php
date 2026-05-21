5350e3590e3ff2d6a53ddaf479f59e56503c3463
Hisham <boussofhisham@gmail.com>
2026-05-21 18:28:03 +0200

    use hasfactory bij alle models

--- app/Models/MediaItem.php
+++ app/Models/MediaItem.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class MediaItem extends Model
 {
+    use HasFactory;
+
     public function reviews() {
         return $this->hasMany(Review::class);
     }
