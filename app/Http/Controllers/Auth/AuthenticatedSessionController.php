86e2f5573ab3e762a403d6e1411c2f64cb98358f
Hisham <boussofhisham@gmail.com>
2026-05-23 18:48:16 +0200

    middleware admin

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,6 +28,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        if (auth()->user()->is_admin) {
+            return redirect()->intended(route('dashboard'));
+        }
+
         return redirect()->intended('/');
     }
 
--- routes/web.php
+++ routes/web.php
@@ -37,14 +37,20 @@ Route::get('/profile/{username}', [ProfileController::class, 'show'])->name('pro
 
 Route::post('/profile/update', [ProfileController::class, 'update'])->middleware('auth')->name('profile.update');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/dashboard', function () {
+        return;
+    })->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
